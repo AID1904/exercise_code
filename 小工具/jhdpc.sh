4dcd39039c325adc70545fe80f51c6f0fa9d09f3
AID1904 <52229808+AID1904@users.noreply.github.com>
2019-08-11 23:56:58 +0800

    Update jhdpc.sh

--- 小工具/jhdpc.sh
+++ 小工具/jhdpc.sh
@@ -8,6 +8,13 @@
 ##### 用户配置区 开始 #####
 
 # 功能：指定文件夹，导入该文件夹内jar包，再进行编译
+# 使用：
+# 1. 添加可执行权限
+# path=`pwd`;sudo chmod +x $path"/"jhdpc.sh
+# 2. 添加软链接
+# path=`pwd`;sudo ln -s $path"/jhdpc.sh" /bin/jhdpc 
+# 3. 使用命令（任意路径）
+# jhdpc xxx.java
 
 # _HADOOP_HOME：hadoop的安装目录 例如：/home/hadoop/app/hadoop-2.9.2
 # _HADOOP_HOME=
